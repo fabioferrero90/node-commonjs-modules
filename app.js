3652e0f04c2d8a9ee8e5f311d598f3337169d41d
fabioferrero90 <fabio.ff90@gmail.com>
2024-12-16 18:15:03 +0100

    feat: hobbies module complete

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const getFullName = require('./modules/names.js')
+const getHobbies = require('./modules/hobbies.js')
+
 
 console.log(getFullName("Fabio", "Ferrero"))
+console.log(getHobbies("Pallavolo", "Nuoto", "Tennis"))
+
---
+++ modules/hobbies.js
@@ -0,0 +1,5 @@
+function getHobbies(hobbyOne, hobbyTwo, hobbyThree) {
+  return { hobbies: [hobbyOne, hobbyTwo, hobbyThree] };
+}
+
+module.exports = getHobbies;
